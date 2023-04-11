892a43abef71b42368c360dcc74e26c2e57f69a4
inga-divra <ingady@gmail.com>
2023-04-11 19:18:06 +0300

    added file for github repository

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+
+  $(".menu a, .go-top").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+  $(".slider-blog__inner").slick({
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="./images/arrow-left.svg" alt="slider left arrow" /></button>',
+    nextArrow:
+      +'<button type="button" class="slick-next"><img src="./images/arrow-right.svg" alt="slider right arrow" /></button>',
+  });
+
+  $(".menu__btn, .menu a").on("click", function () {
+    $(".header__top-inner").toggleClass("header__top-inner--active");
+  });
+
+  var mixer = mixitup(".portfolio__content");
+});
